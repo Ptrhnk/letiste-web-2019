f415b77c6c8b9b181f9308409ce03b90710170be
Petr Hanak <petr.hanak@bcvsolutions.eu>
2019-07-01 19:03:36 +0200

    meta tags repaired

--- layout/meta.js
+++ layout/meta.js
@@ -29,7 +29,7 @@ export default () => (
     <meta property="og:image:type" content="image/png" />
     <meta
       property="og:image:secure_url"
-      content="https://letiste.org/images/og_image.png"
+      content="https://letiste.org/og_image.png"
     />
     <meta property="og:image:width" content="300" />
     <meta property="og:image:height" content="200" />
@@ -39,24 +39,24 @@ export default () => (
     <link
       rel="apple-touch-icon"
       sizes="180x180"
-      href="images/favicons/apple-touch-icon.png"
+      href="favicons/apple-touch-icon.png"
     />
     <link
       rel="icon"
       type="image/png"
       sizes="32x32"
-      href="images/favicons/favicon-32x32.png"
+      href="favicons/favicon-32x32.png"
     />
     <link
       rel="icon"
       type="image/png"
       sizes="16x16"
-      href="images/favicons/favicon-16x16.png"
+      href="favicons/favicon-16x16.png"
     />
-    <link rel="manifest" href="images/favicons/site.webmanifest" />
+    <link rel="manifest" href="favicons/site.webmanifest" />
     <link
       rel="mask-icon"
-      href="images/favicons/safari-pinned-tab.svg"
+      href="favicons/safari-pinned-tab.svg"
       color="#5bbad5"
     />
     <meta name="msapplication-TileColor" content="#da532c" />
