6fb464fd72b34589908f83e5ff9c09d98411789c
Petr Hanak <petr.hanak@bcvsolutions.eu>
2019-11-13 20:00:37 +0100

    Logo rewritten, page for logo testing

--- components/LogoLetisteVar.js
+++ components/LogoLetisteVar.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled, { keyframes } from "styled-components";
+import { useSpring, useTrail, animated, config } from "react-spring";
 
 import { globalBlack } from "../constants";
+import { red } from "ansi-colors";
+
+const lineColor = "rgba(255, 255, 255, 0.8)";
+const linejoin = "round";
+const linecap = "round";
 
 const Container = styled.div`
+  /* background-color: ${globalBlack}; */
   position: absolute;
-  top: 34%;
+  top: 45%;
   left: 50%;
   transform: translate(-50%, -50%);
-  height: 26%;
+  height: 40%;
   display: flex;
   justify-content: center;
   align-items: center;
+  overflow: auto;
 
   @media (max-width: 700px) {
     height: 20%;
@@ -24,114 +32,101 @@ const StyledSvg = styled.svg`
   z-index: 10000;
 `;
 
-const movePoly1 = keyframes`
-  0%, 30% {
-    stroke-dashoffset: 200;
-  }
-  70%, 100% {
-    stroke-dashoffset: 0;
-  }
-`;
-const Polyline1 = styled.polyline`
+const Line = styled(animated.line)`
   fill: none;
-  stroke: ${globalBlack};
-  stroke-linejoin: round;
-  stroke-width: 9px;
-  stroke-linecap: round;
-  stroke-dasharray: 20;
-
-  stroke-dasharray: 200;
-  /* stroke-dashoffset: 0; */
-
-  animation: ${movePoly1} 1.6s ease-in-out;
-  animation-delay: 1s;
-  animation-fill-mode: backwards;
-`;
-
-const movePoly2 = keyframes`
-  0%, 30% {
-    stroke-dashoffset: 220;
-  }
-  70%, 100% {
-    stroke-dashoffset: 0;
-  }
-`;
-const Polyline2 = styled.polyline`
-  fill: none;
-  stroke: ${globalBlack};
-  stroke-linejoin: round;
-  stroke-width: 9px;
-  stroke-linecap: round;
-
+  /* stroke: ${lineColor}; */
+  stroke: rgba(255, 255, 255, .6);
+  stroke-linejoin: ${linejoin};
+  stroke-width: 2px;
+  stroke-linecap: ${linecap};
   stroke-dasharray: 220;
-  /* stroke-dashoffset: 220; */
+  /* opacity: ${({ opacity }) => opacity}; */
+  /* stroke: ${({ blick }) =>
+    blick ? `rgba(255, 255, 255, ${0.8})` : `rgba(255, 255, 255, ${0.0})`}; */
+  opacity: ${({ blick }) => (blick ? 1 : 0)};
 
-  animation: ${movePoly2} 1.6s ease-in-out;
-  animation-delay: 1.5s;
-  animation-fill-mode: backwards;
-`;
-
-const movePoly3 = keyframes`
-  0%, 30% {
-    stroke-dashoffset: 400;
-  }
-  70%, 100% {
-    stroke-dashoffset: 0;
-  }
-`;
-const Polyline3 = styled.polyline`
-  fill: none;
-  stroke: ${globalBlack};
-  stroke-linejoin: round;
-  stroke-width: 9px;
-  stroke-linecap: round;
-  stroke-dasharray: 400;
-  /* stroke-dashoffset: 400; */
-
-  animation: ${movePoly3} 1.8s ease-in-out;
-  animation-delay: 2.1s;
-  animation-fill-mode: backwards;
-`;
-
-const moveLine = keyframes`
-  0%, 30% {
-    stroke-dashoffset: 200;
-  }
-  70%, 100% {
-    stroke-dashoffset: 0;
-  }
-`;
-const CenterLine = styled.line`
-  fill: none;
-  stroke: ${globalBlack};
-  stroke-linejoin: round;
-  stroke-width: 9px;
-  stroke-linecap: round;
-  stroke-dasharray: 200;
   /* stroke-dashoffset: -200; */
-
-  animation: ${moveLine} 2.1s ease-in-out;
-  animation-delay: 2.7s;
-  animation-fill-mode: backwards;
 `;
+
+const lines = [
+  { x1: "163.48", y1: "186.82", x2: "83.99", y2: "175.51" },
+  { x1: "4.5", y1: "186.82", x2: "83.99", y2: "175.51" },
+  { x1: "163.48", y1: "186.82", x2: "83.99", y2: "112.31" },
+  { x1: "4.5", y1: "186.82", x2: "83.99", y2: "112.31" },
+  { x1: "163.48", y1: "186.82", x2: "83.99", y2: "4.5" },
+  { x1: "4.5", y1: "186.82", x2: "83.99", y2: "4.5" },
+  { x1: "83.99", y1: "175.51", x2: "83.99", y2: "4.5" }
+];
 
 const LogoLetiste = () => {
-  const x1 = 4.5;
-  const y1 = 186.82;
-  const x2 = 83.99;
-  const y2 = 155.51;
+  const [flipped, setFlipped] = useState(true);
+  const [flipArr, setFlipArr] = useState(Array(8).fill(true));
+
+  const animations = useSpring({
+    // opacity: flipped ? 1 : 0.8,
+    // strokeWidth: flipped ? `${4}px` : `${3}px`,
+    // strokeDasharray: flipped ? 2 : 5,
+    strokeDashoffset: flipped ? 50 : 55,
+    config: { tension: 500, friction: 2, mass: 1 }
+  });
+
+  const trail = useTrail(lines.length, {
+    config: { tension: 500, friction: 40, mass: 2 },
+    // strokeDashoffset: flipped ? 15 : 55,
+    // strokeDasharray: flipped ? 3 : 200,
+    stroke: flipped ? "turquoise" : "palevioletred"
+  });
+
+  useEffect(() => void setInterval(() => flash(), 200), []);
+  // useEffect(() => void setInterval(() => flip(), 4000), []);
+  const flip = () => {
+    setFlipped(flipped => !flipped);
+  };
+
+  const flash = () => {
+    const index = Math.floor(Math.random() * flipArr.length);
+    const pravda = flipArr[index];
+    setTimeout(() => {
+      flipArr[index] = !pravda;
+      setFlipArr(flipArr.slice());
+      setTimeout(() => {
+        flipArr[index] = pravda;
+        setFlipArr(flipArr.slice());
+      }, Math.floor(Math.random() * 1000 + 100));
+    }, Math.floor(Math.random() * 200));
+  };
 
   return (
     <>
-      <Container>
+      <Container onClick={() => setFlipped(!flipped)}>
         <StyledSvg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 167.98 191.32"
         >
-          <Polyline1 points={`4.5 186.82 83.99 155.51 163.48 186.82`} />
-          <Polyline2 points={`4.5 186.82 83.99 112.31 163.48 186.82`} />
-          <Polyline3 points={`4.5 186.82 83.99 4.5 163.48 186.82`} />
-          <CenterLine x1="83.99" y1="175.51" x2="83.99" y2="4.5" />
+          {/* {lines.map((line, key) => {
+            return (
+              <Line
+                key={key}
+                x1={line.x1}
+                y1={line.y1}
+                x2={line.x2}
+                y2={line.y2}
+                blick={flipArr[key]}
+                style={animations}
+              />
+            );
+          })} */}
+          {trail.map((style, key) => (
+            <Line
+              key={key}
+              x1={lines[key].x1}
+              y1={lines[key].y1}
+              x2={lines[key].x2}
+              y2={lines[key].y2}
+              blick={flipArr[key]}
+              style={style}
+            />
+          ))}
         </StyledSvg>
       </Container>
     </>
---
+++ pages/logo.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+import LogoLetisteVar from "../components/LogoLetisteVar";
+
+const Container = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  height: 100vh;
+  width: 100%;
+  background-color: black;
+  z-index: 10000;
+`;
+
+const logo = () => {
+  return (
+    <Container>
+      <LogoLetisteVar />
+    </Container>
+  );
+};
+
+export default logo;
